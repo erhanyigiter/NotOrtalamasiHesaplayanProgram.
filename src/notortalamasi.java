7d2327d1d6c15430887babf9f3e417da9c098c32
Erhan <146870949+erhanyigiter@users.noreply.github.com>
2023-10-26 23:47:16 +0300

    notortalamasi.java
    farklı bir ödeve uyarladım

--- src/notortalamasi.java
+++ src/notortalamasi.java
@@ -2,40 +2,65 @@ import java.util.Scanner;
 
 public class notortalamasi {
     public static void main(String[] args) {
+        // Değişkenleri atayalım
+        int matematik, fizik, kimya, turkce, muzik;
+        int toplamNot = 0;
 
-        //Değişkenleri atayalım
-        int matematik, fizik, kimya, turkce, tarih, muzik;
-
-        //Scanner sınıfımızı yapalım
+        // Scanner sınıfını oluşturalım
         Scanner inp = new Scanner(System.in);
 
-        //Kullanıcı değerleriini alalım
+        // Kullanıcı değerlerini alalım ve hatalı girişi kontrol edelim
         System.out.print("Matematik Notunuz: ");
         matematik = inp.nextInt();
+        if (matematik < 0 || matematik > 100) {
+            System.out.println("Not sadece 0-100 aralığında olabilir! Notunuz ortalamaya dahil edilmeyecek!");
+        } else {
+            toplamNot += matematik;
+        }
 
         System.out.print("Fizik Notunuz: ");
         fizik = inp.nextInt();
+        if (fizik < 0 || fizik > 100) {
+            System.out.println("Not sadece 0-100 aralığında olabilir! Notunuz ortalamaya dahil edilmeyecek!");
+        } else {
+            toplamNot += fizik;
+        }
 
         System.out.print("Kimya Notunuz: ");
         kimya = inp.nextInt();
+        if (kimya < 0 || kimya > 100) {
+            System.out.println("Not sadece 0-100 aralığında olabilir! Notunuz ortalamaya dahil edilmeyecek!");
+        } else {
+            toplamNot += kimya;
+        }
 
         System.out.print("Türkçe Notunuz: ");
         turkce = inp.nextInt();
-
-        System.out.print("Tarih Notunuz: ");
-        tarih = inp.nextInt();
+        if (turkce < 0 || turkce > 100) {
+            System.out.println("Not sadece 0-100 aralığında olabilir! Notunuz ortalamaya dahil edilmeyecek!");
+        } else {
+            toplamNot += turkce;
+        }
 
         System.out.print("Müzik Notunuz: ");
         muzik = inp.nextInt();
+        if (muzik < 0 || muzik > 100) {
+            System.out.println("Not sadece 0-100 aralığında olabilir! Notunuz ortalamaya dahil edilmeyecek!");
+        } else {
+            toplamNot += muzik;
+        }
+            // Ortalamayı hesaplayalım
+            double ortalama = (double) toplamNot / 5;
 
-        //Dersleri yazdık şimdi ortalamasını alalım
-        int toplam = (matematik + fizik + kimya + turkce + tarih + muzik);
-        double ortalama = toplam / 6.0;
-        System.out.println(ortalama);
+            // Koşullu sonucu yazdıralım
+            String sonuc = (ortalama >= 55) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+            System.out.println("Not Ortalamanız: " + ortalama + "  " + sonuc);
 
-        //Koşullu sonucu yazdıralım
-        String sonuc = (ortalama >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
-        System.out.println("Not Ortalamanız: " + ortalama +  "  "  + sonuc );
+        if (ortalama <55){
+            System.out.print("Açıklama: Not ortalamanız 55'in altında olduğu için kaldınız, daha fazla çalışmalısınız.");
+        }   else {
+            System.out.print("Tebrikler!! Sınıf geçme koşulunu sağlıyorsunuz");
+        }
 
     }
 }
